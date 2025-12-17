09063cf0be608407946a570e8ecfc94491c812ef
Pranjal Poudel <pranjalpaudel62@gmail.com>
2025-12-17 18:32:19 +0545

    more backend update

--- start_server.bat
+++ start_server.bat
@@ -4,5 +4,7 @@ echo Please ensure you have Python installed.
 echo.
 echo Open http://localhost:8000 in your browser.
 echo.
+echo Opening http://localhost:8000 in your browser...
+start http://localhost:8000
 python -m http.server 8000
 pause
